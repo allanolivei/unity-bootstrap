8f9afa0682bbd144d40082a0ae9e09ec427419a8
Allan Oliveira <allanolivei@gmail.com>
2017-08-31 20:40:28 -0300

    janela para substituicao de materiais

---
+++ Scripts/Editor/MayaPostProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MayaPostProcessor : AssetPostprocessor
+{
+    void OnPreprocessModel ()
+    {
+        if (assetPath.Contains("Maya"))
+        {
+            ModelImporter modelImporter = assetImporter as ModelImporter;
+            modelImporter.materialName = ModelImporterMaterialName.BasedOnMaterialName;
+        }
+    }
+}
