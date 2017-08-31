df1a2a1da70335cdad12f5859cc796dcd88e977d
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2017-08-31 08:59:26 -0300

    SplashScript atualizado para padrão de jogos da ANEEL

--- Scripts/Utils/SplashScript.cs
+++ Scripts/Utils/SplashScript.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class SplashScript : MonoBehaviour {
+public class SplashScript : MonoBehaviour
+{
 
-    public GameObject[] objts;
-    
+    public CanvasGroup[] objts;
+    public string sceneName;
+
     void Start()
     {
         StartCoroutine("Splash");
@@ -15,40 +15,32 @@ public class SplashScript : MonoBehaviour {
 
     IEnumerator Splash()
     {
-        yield return new WaitForSeconds(1);
-        StartCoroutine(WaitForSecondsUnscale( 1.0f, objts[0],1f));
-        yield return new WaitForSeconds(2);
-        StartCoroutine(WaitForSecondsUnscale(1.0f, objts[0], 0));
-        yield return new WaitForSeconds(1);
-        StartCoroutine(WaitForSecondsUnscale(1.0f, objts[1], 1f));
-        yield return new WaitForSeconds(2);
-        StartCoroutine(WaitForSecondsUnscale(1.0f, objts[1], 0));
+        for (int i = 0; i < objts.Length; i++)
+        {
+            yield return new WaitForSeconds(1);
+            StartCoroutine(WaitForSecondsUnscale(1.0f, objts[i], 1f));
+            yield return new WaitForSeconds(2);
+            StartCoroutine(WaitForSecondsUnscale(1.0f, objts[i], 0));
+        }
         yield return new WaitForSeconds(1.5f);
-        
-        SceneManager.LoadScene("MainMenu");
-        
+
+        SceneManager.LoadScene(sceneName);
     }
 
-    IEnumerator WaitForSecondsUnscale(float duration , GameObject go, float toDo)
+    IEnumerator WaitForSecondsUnscale(float duration, CanvasGroup cGroup, float toDo)
     {
         float initTime = Time.unscaledTime;
-        Color a = go.GetComponent<Image>().color;
-        float init = a.a;
-        //Debug.Log(a);
+        float init = cGroup.alpha;
         while (true)
         {
-            
             float percent = (Time.unscaledTime - initTime) / duration;
-
-            go.GetComponent<Image>().color = Color.Lerp(new Color(1,1,1,init), new Color(1, 1, 1, toDo), percent);
-           // Debug.Log(new Color(1, 1, 1, init) +" <<<<<<COLOR>>>>  "+ new Color(1, 1, 1, toDo));
-            if (percent >= 1.0f) 
+            cGroup.alpha = Mathf.Lerp(init, toDo, percent);
+            if (percent >= 1.0f)
             {
-                a = new Color(1, 1, 1, toDo);
-                    break;
+                cGroup.alpha = toDo;
+                break;
             }
             yield return null;
         }
-
     }
 }
