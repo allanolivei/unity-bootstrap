e47a619545adfa82844bb3c12d81743be21a1d00
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2017-09-15 13:33:12 -0300

    FPSDisplay reposicionável e com toggle

--- Scripts/Utils/FPSDisplay.cs
+++ Scripts/Utils/FPSDisplay.cs
@@ -1,38 +1,49 @@
 ﻿
 using UnityEngine;
-using System.Collections;
 
 public class FPSDisplay : MonoBehaviour
 {
 
 #if UNITY_EDITOR// || !(UNITY_ANDROID && UNITY_IOS)
 
+    [Header("Aperte = (igual) para mostrar/esconder o display")]
+    [Space]
+    public Vector2 displayPosition;
+
     float deltaTime = 0.0f;
     private const string format = "{0:0.0} ms ({1:0.} fps)\nScreen: {2}x{3} : {4}dpi\nTimeScale: {5}";
+    public bool showDisplay = true;
 
     void Update()
     {
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            showDisplay = !showDisplay;
+        }
     }
 
     void OnGUI()
     {
-        //int w = Screen.width, h = Screen.height;
+        if (showDisplay)
+        {
+            //int w = Screen.width, h = Screen.height;
 
-        //GUIStyle style = new GUIStyle();
+            //GUIStyle style = new GUIStyle();
 
-       // Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        //style.alignment = TextAnchor.UpperLeft;
-        //style.fontSize = h * 2 / 100;
-        //style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        //string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        //GUI.Label(rect, text, style);
-        string text = string.Format(format, msec, fps, Screen.width, Screen.height, Screen.dpi, Time.timeScale);
-        GUI.Box( new Rect(0, Screen.height-50, 200, 50), text);
+            // Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            //style.alignment = TextAnchor.UpperLeft;
+            //style.fontSize = h * 2 / 100;
+            //style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            //string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            //GUI.Label(rect, text, style);
+            string text = string.Format(format, msec, fps, Screen.width, Screen.height, Screen.dpi, Time.timeScale);
+            GUI.Box(new Rect(displayPosition.x, displayPosition.y, 200, 50), text);
 
-        //GUILayout.Label("Screen: " + Screen.width + "x" + Screen.height + " : " + Screen.dpi + "dpi");
+            //GUILayout.Label("Screen: " + Screen.width + "x" + Screen.height + " : " + Screen.dpi + "dpi");
+        }
     }
 #endif
 
