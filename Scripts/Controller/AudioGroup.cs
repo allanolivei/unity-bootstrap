7d945c3bf5ad6e33816fb76691244464428c9a80
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2017-09-15 11:42:36 -0300

    SoundManager com flexibilidade de grupos e snapshots

---
+++ Scripts/Controller/AudioGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioGroup {
+
+    [Tooltip("Use o nome como referência no script")]
+    public string name;
+    [Tooltip("Caminho exato do AudioGroup no Mixer. Ex.: UI/TrilhaSonora")]
+    public string path;
+}
